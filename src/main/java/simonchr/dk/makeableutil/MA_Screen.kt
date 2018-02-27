7777175fc64675bfe30965d1010210d1799b9d76
Simon Christensen <simon@makeable.dk>
2018-02-27 19:16:41 +0100

    Initial Commit

---
+++ src/main/java/simonchr/dk/makeableutil/MA_Screen.kt
@@ -0,0 +1,16 @@
+package simonchr.dk.makeableutil
+
+import android.content.res.Resources
+
+/**
+ * Created by simonchristensen on 27/02/2018.
+ */
+class MA_Screen(){
+    fun getScreenWidth(): Int {
+        return Resources.getSystem().getDisplayMetrics().widthPixels
+    }
+
+    fun getScreenHeight(): Int {
+        return Resources.getSystem().getDisplayMetrics().heightPixels
+    }
+}
